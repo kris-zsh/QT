78a5668de14a8f237974dc52f0a222a56ed48b12
kris hao <76926060+kris-zsh@users.noreply.github.com>
2023-02-25 14:20:46 +0800

    使用两个按钮实现弹出窗口和关闭窗口

---
+++ 03_my_open_close/my_open_close.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2023-02-25T14:03:40
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = my_open_close
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
---
+++ 03_my_open_close/widget.cpp
@@ -0,0 +1,35 @@
+#include "widget.h"
+#include <QPushButton>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    QPushButton* open = new QPushButton("open", this);
+    QPushButton* close = new QPushButton("close", this);
+
+    open->setFixedSize(100,100);
+    close->setFixedSize(100,100);
+    this->setFixedSize(600,400);
+
+    close->move(100,0);
+
+    close->setEnabled(false);
+    QPushButton* windows = new QPushButton("hello");
+
+    connect(open, &QPushButton::clicked, [=]{
+        windows->show();
+        close->setEnabled(true);
+        open->setEnabled(false);
+    });
+
+    connect(close,&QPushButton::clicked, [=]{
+        windows->close();
+        close->setEnabled(false);
+        open->setEnabled(true);
+    });
+}
+
+Widget::~Widget()
+{
+
+}
