83200bf575c1bfde6c49045a7f5e6c343250e4e0
kris hao <76926060+kris-zsh@users.noreply.github.com>
2023-02-25 14:25:03 +0800

    使用一个按钮来实现窗口的开和关，并更改按钮的文字

--- 03_my_open_close/widget.cpp
+++ 03_my_open_close/widget.cpp
@@ -5,28 +5,36 @@ Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
     QPushButton* open = new QPushButton("open", this);
-    QPushButton* close = new QPushButton("close", this);
+//    QPushButton* close = new QPushButton("close", this);
 
     open->setFixedSize(100,100);
-    close->setFixedSize(100,100);
+//    close->setFixedSize(100,100);
     this->setFixedSize(600,400);
 
-    close->move(100,0);
+//    close->move(100,0);
 
-    close->setEnabled(false);
+//    close->setEnabled(false);
     QPushButton* windows = new QPushButton("hello");
 
     connect(open, &QPushButton::clicked, [=]{
-        windows->show();
-        close->setEnabled(true);
-        open->setEnabled(false);
+        if(open->text() == "open"){
+            windows->show();
+            open->setText("close");
+        }
+        else{
+            windows->close();
+            open->setText("open");
+        }
+
+//        close->setEnabled(true);
+//        open->setEnabled(false);
     });
 
-    connect(close,&QPushButton::clicked, [=]{
-        windows->close();
-        close->setEnabled(false);
-        open->setEnabled(true);
-    });
+//    connect(close,&QPushButton::clicked, [=]{
+//        windows->close();
+//        close->setEnabled(false);
+//        open->setEnabled(true);
+//    });
 }
 
 Widget::~Widget()
