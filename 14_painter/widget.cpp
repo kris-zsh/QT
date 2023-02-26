74d0618704b4c49c91b3ee6fae0c30f99943f9d3
kris hao <76926060+kris-zsh@users.noreply.github.com>
2023-02-26 21:27:08 +0800

    high level painter

--- 14_painter/widget.cpp
+++ 14_painter/widget.cpp
@@ -18,15 +18,32 @@ Widget::~Widget()
 
 void Widget::paintEvent(QPaintEvent *event)
 {
+//    QPainter painter(this);
+
+
+//    painter.setBrush(Qt::cyan);
+//    painter.setPen(Qt::darkBlue);
+
+//    painter.drawLine(QPoint(0,0), QPoint(100,100));
+//    painter.drawEllipse(QPoint(100,100),30,30);
+//    painter.drawRect(QRect(QPoint(200,400), QPoint(600,800)));
+
+//    painter.drawText(QRect(QPoint(600, 300), QPoint(700,400)), "青春没有售价，硬座直达拉萨");
+
+    /////////////////////////高级设置
     QPainter painter(this);
 
+    painter.drawEllipse(QPoint(100,100),200,200);
+    painter.setRenderHint(QPainter::HighQualityAntialiasing);
+    painter.drawEllipse(QPoint(200,200),200,200);
 
-    painter.setBrush(Qt::cyan);
-    painter.setPen(Qt::darkBlue);
+    painter.drawRect(100,200,100,100);
 
-    painter.drawLine(QPoint(0,0), QPoint(100,100));
-    painter.drawEllipse(QPoint(100,100),30,30);
-    painter.drawRect(QRect(QPoint(200,400), QPoint(600,800)));
+    painter.save();
+    painter.translate(100,100);
+    painter.drawRect(100,200,100,100);
 
-    painter.drawText(QRect(QPoint(600, 300), QPoint(700,400)), "青春没有售价，硬座直达拉萨");
+    painter.restore();
+    painter.translate(100,100);
+    painter.drawRect(100,200,100,100);
 }
